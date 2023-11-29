1c98f9c70d4681a5e1dfcb1f0390d9f462089771
Brett Stevens <152364053+brettjstevens@users.noreply.github.com>
2023-11-29 21:45:24 +0800

    Add files via upload

---
+++ MXEN3004_assignment/examlpe.m
@@ -0,0 +1,14 @@
+
+%replace 12345678 wiht your student number
+N=19407580
+
+[k,a,b,c,d,wl,wh,ev]=systems_generator(N);
+
+%k,a,b,c,d parameters of the system
+
+%wl minimum gain crossover frequency
+%wh maximum gain crossover frequency
+%ev maximum velocity error 
+
+
+
